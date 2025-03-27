dcefe1432cb8e862aa39bf151715057400dd6391
Fillsogood <jdd05273@naver.com>
2025-03-27 13:59:38 +0900

    chore-TypeScript 변경

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -8,7 +10,7 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('당근 백엔드 서버 실행 중!');
 });
 
