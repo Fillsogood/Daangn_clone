53eb8f8eff8b4fe8ddb7b01a2fb5ea161399735c
Fillsogood <jdd05273@naver.com>
2025-03-28 14:17:42 +0900

    feat-카카오 소셜 로그인 기능

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import * as authController from '../controllers/auth.controller';
 import { verifyToken } from '../middlewares/auth.middleware';
+import { kakaoLogin, kakaoCallback } from '../controllers/auth.controller';
+
 
 const router = express.Router();
 
@@ -9,5 +11,8 @@ router.post('/login', authController.login);
 router.get('/me', verifyToken, authController.me);
 router.post('/refresh', authController.refresh);
 router.post('/logout', authController.logout);
+router.get('/kakao', authController.kakaoLogin);
+router.get('/kakao/callback', authController.kakaoCallback);
+
 
 export default router;
